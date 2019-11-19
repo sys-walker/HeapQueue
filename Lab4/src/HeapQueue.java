467161bc84702b8aaa035e17880c297311874f28
pere <pere.rollon@gmail.com>
2019-11-19 18:35:25 +0100

    kmob

--- Lab4/src/HeapQueue.java
+++ Lab4/src/HeapQueue.java
@@ -1,10 +1,18 @@
 
+import java.time.Instant;
 import java.util.*;
 
 public class HeapQueue<V,P extends Comparable<? super P>> implements PriorityQueue<V,P> {
 
     private final ArrayList<TSPair<V,P>> pairs = new ArrayList<>();
+
+    private int heapSize;
     private long nextTimeStamp = 0L;
+
+
+    public HeapQueue() {
+        this.heapSize=0;
+    }
 
 
     private static class TSPair<V, P extends Comparable<? super P>> implements Comparable<TSPair<V,P>>  {
@@ -23,11 +31,39 @@ public class HeapQueue<V,P extends Comparable<? super P>> implements PriorityQue
             return 0;
         }
 
+        @Override
+        public String toString() {
+            return "["+value+","+priority+",("+")]";
+        }
     }
 
     @Override
     public void add(V value, P priority) {
+        nextTimeStamp = (new Date().getTime()/1000L);
 
+        TSPair<V,P> element = new TSPair<>(value,priority,nextTimeStamp);
+        pairs.add(element);
+        heapSize+=1;
+        makeMaxHeap(pairs,heapSize-1);
+
+    }
+
+    private void makeMaxHeap(ArrayList<TSPair<V,P>> pairs, int i) {
+
+        P auxP = pairs.get(i).priority;
+        TSPair<V,P> auxN = pairs.get(i);
+        while (hasParent(i) && comparadoreishon(auxP,pairs,i)>0){
+            pairs.set(i,pairs.get(parent(i)));
+            i = parent(i);
+        }
+        pairs.set(i,auxN);
+    }
+
+    private int comparadoreishon(P auxP, ArrayList<TSPair<V,P>> pairs,int i) {
+        //System.out.println("prioritat REF "+auxP);
+        //System.out.println("prioritat Nxt "+pairs.get(parent(i)).priority);
+        //System.out.println("---------------------->"+auxP.compareTo(pairs.get(parent(i)).priority));
+        return auxP.compareTo(pairs.get(parent(i)).priority);
     }
 
     @Override
@@ -42,8 +78,10 @@ public class HeapQueue<V,P extends Comparable<? super P>> implements PriorityQue
 
     @Override
     public int size() {
-        return 0;
+        return heapSize;
     }
+
+
 
     public static int parent(int index){return index/2;}
     public static int left(int index){return 2*index+1;}
@@ -53,4 +91,16 @@ public class HeapQueue<V,P extends Comparable<? super P>> implements PriorityQue
     public boolean hasParent(int index){return index > 0;}
     public boolean hasLeft(int index){return isValid(left(index));}
     public boolean hasRight(int index){return isValid(right(index));}
+
+    @Override
+    public String toString() {
+        Object[] Queue = pairs.toArray();
+        StringBuilder s = new StringBuilder("Queue -");
+        for (int i = 0; i < Queue.length; i++) {
+            s.append(Queue[i].toString()).append("-");
+
+
+        }
+        return s.toString();
+    }
 }
--- Lab4/src/Main.java
+++ Lab4/src/Main.java
@@ -2,5 +2,21 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("ES CONSIDERA QUE LES PRIORITATAS NO ES REPETEIXEN ");
+        HeapQueue<String,Integer> q = new HeapQueue<>();
+        q.add("a",5);
+        System.out.println(q.toString());
+        q.add("a",2);
+        System.out.println(q.toString());
+        q.add("a",9);
+        System.out.println(q.toString());
+        q.add("a",8);
+        System.out.println(q.toString());
+        q.add("a",6);
+        System.out.println(q.toString());
+        q.add("a",7);
+        System.out.println(q.toString());
+        System.out.println("\n size of q "+q.size());
+
     }
 }
