0091e996a70ee53cedf91400a8bbf6facb5c71af
pere <pere.rollon@gmail.com>
2019-11-22 18:47:56 +0100

    Ara amb carpeta Test

---
+++ Lab4/test/HeapQueueTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeapQueueTest {
+
+    @Test
+    void parent() {
+    }
+
+    @Test
+    void left() {
+    }
+
+    @Test
+    void right() {
+    }
+
+    @Test
+    void isValid() {
+    }
+
+    @Test
+    void hasParent() {
+    }
+
+    @Test
+    void hasLeft() {
+    }
+
+    @Test
+    void hasRight() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void element() {
+    }
+
+    @Test
+    void size() {
+    }
+}
